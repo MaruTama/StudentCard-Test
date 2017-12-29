d3308b35e1524c2ab0d1b7d8123eee74b396a465
MaruTama <b1403525@planet.kanazawa-it.ac.jp>
2017-12-30 06:17:27 +0900

    入力された文字列を16文字n行に整形できるようにした。

--- app/src/main/java/com/example/tama/studentcardsample/WriteNfcfFragment.java
+++ app/src/main/java/com/example/tama/studentcardsample/WriteNfcfFragment.java
@@ -1,5 +1,6 @@
 package com.example.tama.studentcardsample;
 
+import android.arch.lifecycle.LifecycleOwner;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.Editable;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by tama on 2017/12/06.
@@ -20,6 +22,8 @@ public class WriteNfcfFragment extends NfcFeliCaTagFragment implements TextWatch
     private NfcWriter nfcWriter = new NfcWriter();
 
     private EditText edittext;
+    private TextView status;
+    private TextView preview;
 
     private byte[] system_code = new byte[]{(byte) 0xfe,(byte) 0x00};
 
@@ -27,8 +31,9 @@ public class WriteNfcfFragment extends NfcFeliCaTagFragment implements TextWatch
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_write, container, false);
+        status   = (TextView) view.findViewById(R.id.status_text);
+        preview  = (TextView) view.findViewById(R.id.preview_text);
         edittext = (EditText) view.findViewById(R.id.editText);
-
         //リスナーを登録
         edittext.addTextChangedListener(this);
 
@@ -77,10 +82,7 @@ public class WriteNfcfFragment extends NfcFeliCaTagFragment implements TextWatch
      */
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
-        String str = String.valueOf(s);
-        if (str.matches("\\p{ASCII}*")) {
-            System.out.println("Ascii charcter only.");
-        }
+
     }
 
     /** 最後にこのメソッドが呼び出される
@@ -88,5 +90,57 @@ public class WriteNfcfFragment extends NfcFeliCaTagFragment implements TextWatch
      */
     @Override
     public void afterTextChanged(Editable s) {
+//        edittext.setText(shapText(String.valueOf(s)));
+        if(!isAcsii(String.valueOf(s))) status.setText("文字列はASCIIコード文字のみで構成してください。");
+        else status.setText("");
+
+        String str    = String.valueOf(s);
+        String shaped = shapText(str);
+        preview.setText(shaped);
+        // 改行文字は文字数に加算しない
+        status.setText("length : " + String.valueOf(shaped.replace("\n", "").length()));
+    }
+
+    // 渡された文字列がASCII文字のみから構成されるか判定
+    // http://typea.info/tips/wiki.cgi?page=Java+ASCII%CA%B8%BB%FA%CE%F3%A4%AB%A4%CE%C8%BD%C4%EA
+    private boolean isAcsii(String str){
+        if (str.matches("\\p{ASCII}*")) return true;
+        else                                  return false;
+    }
+// 現状、パディングしてもedittextに反映されず、160文字以上になっても制限が効かないので、あとで修正する。
+// ↑このバグも一因として一旦textviewに表示している
+    // 渡された文字列を16文字n行に成形して返す。
+    // 16文字以内で改行されている場合は16文字まで空白でパディングする。
+    private String shapText(String str){
+        String shaped = "";
+        // 改行で分解する
+        String[] sArray = str.split("\n");
+        for(String s : sArray){
+            for(int i=0; i<s.length(); i=i+16){
+                if(i+16 < s.length()){
+                    shaped += s.substring(i,i+16) + "\n";
+                }
+                else{
+                    shaped += padText(s.substring(i,s.length())) + "\n";
+                }
+            }
+        }
+        Log.d(TAG,shaped);
+        return shaped;
+    }
+    // 16文字未満のときパディングする
+    // 16文字以上のときはそのまま返す
+    private String padText(String str){
+        String s = "";
+        if(s.length() < 16) return str+getSpace(16-str.length());
+        else return str;
+    }
+    // 指定された長さの空白を返す
+    private String getSpace(int n){
+        String str = "";
+        for(int i=0; i<n; i++){
+            str += " ";
+        }
+        return str;
     }
 }
